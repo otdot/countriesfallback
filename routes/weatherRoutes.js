929a506c3db1d142b9c8f4873542653971c386ef
otdot <otto.mursula@edu.bc.fi>
2022-09-13 19:35:21 +0300

    fixed weather api implementation

--- routes/weatherRoutes.js
+++ routes/weatherRoutes.js
@@ -5,9 +5,9 @@ const { WEATHER_API_KEY } = require("../utils/config");
 router.get("/:latlng", async (req, res) => {
   const [lat, lon] = req.params.latlng.split("_");
 
-  const weather =
-    await axios.get(`https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lon}&appid=${WEATHER_API_KEY}&units=metric
-      `);
+  const weather = await axios.get(
+    `https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lon}&appid=${WEATHER_API_KEY}&units=metric`
+  );
   res.send(weather.data);
 });
 

1dec4b594e8c71de8a584cc07c4057880a5c0ebc
otdot <otto.mursula@edu.bc.fi>
2022-09-30 19:51:09 +0300

    added netlify func

--- index.js
+++ index.js
@@ -1,23 +1,5 @@
-require("dotenv").config();
-const express = require("express");
-const morgan = require("morgan");
 const http = require("http");
-const cors = require("cors");
-const weatherRouter = require("./routes/weatherRoutes");
-const countryRouter = require("./routes/countryRoutes");
-
-const app = express();
-
-app.use(express.json());
-app.use(cors());
-// morgan.token("body", (req, res) => {
-//   return JSON.stringify(req.body);
-// });
-app.use(morgan("dev"));
-app.use(express.static("build"));
-
-app.use("/api/countries", countryRouter);
-app.use("/api/weather", weatherRouter);
+const app = require("./app");
 
 const server = http.createServer(app);
 

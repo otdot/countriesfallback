736fe916ef146b8eaf1e45473cb5858fe1bf6d43
otdot <otto.mursula@edu.bc.fi>
2022-09-30 22:28:59 +0300

    modified

--- netlify/functions/api.js
+++ netlify/functions/api.js
@@ -1,4 +1,27 @@
 const serverless = require("serverless-http");
-const app = require("../../app");
+require("dotenv").config();
+const express = require("express");
+const morgan = require("morgan");
+const cors = require("cors");
+const path = require("path");
+const pingRouter = require("./routes/pingRouter");
+const weatherRouter = require("./routes/weatherRoutes");
+const countryRouter = require("./routes/countryRoutes");
+const app = express();
+
+app.use(express.json());
+app.use(cors());
+// morgan.token("body", (req, res) => {
+//   return JSON.stringify(req.body);
+// });
+app.use(morgan("dev"));
+app.use(express.static("build"));
+
+app.use("/.netlify/functions/api/countries", countryRouter);
+app.use("/.netlify/functions/api/weather", weatherRouter);
+app.use("/", (req, res) =>
+  res.sendFile(path.join(__dirname, "../../dist/index.html"))
+);
+app.use("", pingRouter);
 
 module.exports.handler = serverless(app);

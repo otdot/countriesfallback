cbf3fea5f33a401cd906421c49a4ce815ead4b48
otdot <otto.mursula@edu.bc.fi>
2022-10-03 10:21:45 +0300

    added

--- app.js
+++ app.js
@@ -2,7 +2,6 @@ require("dotenv").config();
 const express = require("express");
 const morgan = require("morgan");
 const cors = require("cors");
-const pingRouter = require("./routes/pingRouter");
 const weatherRouter = require("./routes/weatherRoutes");
 const countryRouter = require("./routes/countryRoutes");
 
@@ -18,6 +17,5 @@ app.use(express.static("build"));
 
 app.use("/api/countries", countryRouter);
 app.use("/api/weather", weatherRouter);
-app.use("", pingRouter);
 
 module.exports = app;
---
+++ readme.md
@@ -0,0 +1,7 @@
+## Countries and weathers app.
+
+    Express/React app
+
+    for local use clone and run [Countries Frontend](https://github.com/otdot/countriesFall) and update the baseUrl's in src/services/countryService.js and src/services/weatherService.js
+
+    deployed at [Countries and Weathers](https://countriesandweathers.netlify.app/)
